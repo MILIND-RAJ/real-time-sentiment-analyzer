243f9d9cb2754bdeb631a7b5335f45dedbc2598a
MILIND RAJ <milindraj003@gmail.com>
2021-06-15 12:21:25 +0530

    Update api.py

--- api.py
+++ api.py
@@ -4,7 +4,10 @@ import tweepy as tw
 import pandas as pd
 import re
 import nltk
-import teq
+try:
+  nltk.data.find('vader_lexicon')
+except LookupError:
+  nltk.download('vader_lexicon')
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import praw
 
