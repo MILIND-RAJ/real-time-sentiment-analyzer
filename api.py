8d2d6136c826fbb10a85f250914c1b37e7aef326
MILIND RAJ <milindraj003@gmail.com>
2021-06-15 13:16:26 +0530

    Update api.py

--- api.py
+++ api.py
@@ -4,11 +4,6 @@ import tweepy as tw
 import pandas as pd
 import re
 import nltk
-try:
-    nltk.data.find('/app/nltk_data/vader_lexicon')
-except LookupError:
-    nltk.download('vader_lexicon')
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import praw
 
 def cleanResume(resumeText):
@@ -28,11 +23,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    nltk.download('vader_lexicon')
     return render_template('home.html')
 
 @app.route('/result',methods = ['POST', 'GET'])
 def result():
     if request.method == 'POST':
+        from nltk.sentiment.vader import SentimentIntensityAnalyzer
         result = request.form['file_name']
         access_token="1238649645615566848-KxUwD20cSaLCzMxKdBu2ECWqsxgSnr"
         access_token_secret="g1jNl3iJU34oGfw2W5ixLDVF34jnPhFSZCFdbRRTBZcfw"
